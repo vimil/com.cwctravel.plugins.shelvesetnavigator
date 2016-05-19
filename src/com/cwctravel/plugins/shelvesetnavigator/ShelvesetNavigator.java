ec5c3e4a63921b2ef2ebe9ee2632c3a67005fc57
vimil <vimil.saju@gmail.com>
2016-05-19 00:35:16 -0700

    changes to refresh on open of node

--- src/com/cwctravel/plugins/shelvesetnavigator/ShelvesetNavigator.java
+++ src/com/cwctravel/plugins/shelvesetnavigator/ShelvesetNavigator.java
@@ -1,14 +1,18 @@
 package com.cwctravel.plugins.shelvesetnavigator;
 
 import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.jface.viewers.IOpenListener;
+import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ITreeViewerListener;
+import org.eclipse.jface.viewers.OpenEvent;
 import org.eclipse.jface.viewers.TreeExpansionEvent;
+import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.ui.navigator.CommonNavigator;
 
 import com.cwctravel.plugins.shelvesetnavigator.model.ShelvesetGroupItemContainer;
 import com.cwctravel.plugins.shelvesetnavigator.model.ShelvesetItem;
 
-public class ShelvesetNavigator extends CommonNavigator implements ITreeViewerListener {
+public class ShelvesetNavigator extends CommonNavigator implements ITreeViewerListener, IOpenListener {
 	private ShelvesetGroupItemContainer shelvesetGroupItemContainer;
 
 	public ShelvesetNavigator() {
@@ -17,6 +21,7 @@ public class ShelvesetNavigator extends CommonNavigator implements ITreeViewerLi
 
 	protected IAdaptable getInitialInput() {
 		getCommonViewer().addTreeListener(this);
+		getCommonViewer().addOpenListener(this);
 		return shelvesetGroupItemContainer;
 	}
 
@@ -32,6 +37,24 @@ public class ShelvesetNavigator extends CommonNavigator implements ITreeViewerLi
 			ShelvesetItem shelvesetItem = (ShelvesetItem) element;
 			if (!shelvesetItem.isChildrenRefreshed()) {
 				shelvesetItem.refreshShelvesetFileItems(true);
+			}
+		}
+
+	}
+
+	@Override
+	public void open(OpenEvent event) {
+		ISelection selection = event.getSelection();
+		if (selection instanceof TreeSelection) {
+			TreeSelection treeSelection = (TreeSelection) selection;
+			if (!selection.isEmpty()) {
+				Object selectedObject = treeSelection.getFirstElement();
+				if (selectedObject instanceof ShelvesetItem) {
+					ShelvesetItem shelvesetItem = (ShelvesetItem) selectedObject;
+					if (!shelvesetItem.isChildrenRefreshed()) {
+						shelvesetItem.refreshShelvesetFileItems(true);
+					}
+				}
 			}
 		}
 
